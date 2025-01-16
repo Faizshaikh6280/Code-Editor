d2906ffef11de043d3d8f4a11cbac15f1350420c
Faiz Alam <faizalam2432005@gmail.com>
2025-01-16 23:12:14 +0530

    Clerk Auth and Convex DB setedup

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+//https://champion-cod-38.clerk.accounts.dev
+export default {
+  providers: [
+    {
+      domain: "https://champion-cod-38.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
+import ConvexClientProvider from "@/_components/ConvexClientProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +31,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <ConvexClientProvider>{children}</ConvexClientProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
