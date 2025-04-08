293a9b0ccd45acd6972e15b7721f0c6dc65a3219
Faiz Alam <faizalam2432005@gmail.com>
2025-04-09 00:32:08 +0530

    Code Kraze Completed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ function Footer() {
         <div className="flex flex-col md:flex-row items-center justify-between gap-4">
           <div className="flex items-center gap-2 text-gray-400">
             <Blocks className="size-5" />
-            <span>Built for developers, by Faiz Alam</span>
+            <span>Built for developers, by Faiz Alam & Faham</span>
           </div>
           <div className="flex items-center gap-6">
             <Link
